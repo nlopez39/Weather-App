ea9bd3fd2aa7138cba134994a2d4d2ee5d66ff6a
Nelly Lopez <nellymlopez3@gmail.com>
2024-01-28 11:57:40 -0800

    added localstorage and fixed api call to putput all six days

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -25,90 +25,71 @@ function findFiveDayForecast() {
       return response.json();
     })
     .then(function (data) {
+      console.log(data);
+      // console.log(data.city.name);
       var forecastData = [];
-      var daysProcessed;
-      for (var i = 0; i < data.list.length; i += 8) {
-        forecastData.push({
-          date: data.list[i].dt_txt,
-          temperature1: parseInt(
-            ((parseInt(data.list[i].main.temp) - 273.15) * 9) / 5 + 32
-          ), //this will get the temperature
-          //   icon: data.weather[0].icon, //gets icon
-          wind: data.list[i].wind.speed,
-          humidity: data.list[i].main.humidity,
-        });
-        daysProcessed++;
-        if (daysProcessed == 5) {
-          break;
+      for (var i = 0; i < data.list.length; i++) {
+        var dateVar = data.list[i].dt_txt.slice(0, 10);
+        console.log(dateVar);
+        ///if we have reached the last eleement  or if data.list[i+1] an element exists or if we hit the last eleemt  && data Var !===exists first
+        if (
+          i === data.list.length - 1 ||
+          (data.list[i + 1] && dateVar !== data.list[i + 1].dt_txt.slice(0, 10))
+        ) {
+          // if (dateVar !== data.list[i + 1].dt_txt.slice(0, 10)) {
+          forecastData.push({
+            name: data.city.name,
+            date: dateVar,
+            temperature1: parseInt(
+              ((parseInt(data.list[i].main.temp) - 273.15) * 9) / 5 + 32
+            ), //this will get the temperature
+            //   icon: data.weather[0].icon, //gets icon
+            wind: data.list[i].wind.speed,
+            humidity: data.list[i].main.humidity,
+          });
+          // }
         }
       }
       return forecastData;
     });
 }
 
-function findCity() {
-  var city = userInput.value;
-  // console.log(city);
-
-  var requestUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city +
-    "&appid=" +
-    apiKey;
-
-  return fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      return {
-        cityName: data.name,
-        temperature: data.main.temp, //this will get the temperature
-        icon: data.weather[0].icon, //gets icon
-        wind: data.wind.speed,
-        humidity: data.main.humidity,
-      };
-    });
-}
-
 searchBtn.addEventListener("click", function () {
-  findCity().then(function ({ cityName, temperature, icon, wind, humidity }) {
-    console.log(cityName);
-    // var createDiv = document.createElement("div");
-    var createHistory = document.createElement("button");
-    createHistory.textContent = cityName;
-    //append button to the div
-    buttonList.appendChild(createHistory);
-    // createDiv.append(createHistory);
-    //change temp, wind and humidity from Atlanta
-    var nameContainer1 = document.querySelector(".col h1");
-    nameContainer1.textContent = cityName;
-    var tempContainer1 = document.querySelector(".temp");
-    tempContainer1.textContent =
-      "Temp: " +
-      parseInt(((parseInt(temperature) - 273.15) * 9) / 5 + 32) +
-      "°F";
-    var windContainer1 = document.querySelector(".wind");
-    windContainer1.textContent = "Wind: " + wind + " MPH";
-    var humContainer1 = document.querySelector(".humidity");
-    humContainer1.textContent = "Humidity: " + humidity + "%";
-    //reset form
-    formEl.reset();
-  });
   findFiveDayForecast().then(function (forecastData) {
-    forecastData.forEach(function (forecast, index) {
-      var forecastContainer =
-        document.querySelectorAll(".col.me-4.border")[index];
+    var createHistory = document.createElement("button");
 
-      var dateContainer = forecastContainer.querySelector("ul li:nth-child(1)");
-      dateContainer.textContent = "Date: " + forecast.date;
-      var tempContainer = forecastContainer.querySelector("ul li:nth-child(3)");
-      tempContainer.textContent = "Temp: " + forecast.temperature1 + "°F";
-      var windContainer = forecastContainer.querySelector("ul li:nth-child(4)");
-      windContainer.textContent = "Wind: " + forecast.wind + " MPH";
-      var humContainer = forecastContainer.querySelector("ul li:nth-child(5)");
-      humContainer.textContent = "Humidity: " + forecast.humidity + "%";
+    forecastData.forEach(function (forecast, index) {
+      createHistory.textContent = forecast.name;
+      var nameContainer1 = document.querySelector(".col h1");
+      if (nameContainer1) {
+        nameContainer1.textContent = forecast.name;
+      }
+      var forecastContainer = document.querySelectorAll(".forecast")[index];
+
+      if (forecastContainer) {
+        var dateContainer =
+          forecastContainer.querySelector("ul li:nth-child(1)");
+        dateContainer.textContent = "Date: " + forecast.date;
+        var tempContainer =
+          forecastContainer.querySelector("ul li:nth-child(3)");
+        tempContainer.textContent = "Temp: " + forecast.temperature1 + "°F";
+        var windContainer =
+          forecastContainer.querySelector("ul li:nth-child(4)");
+        windContainer.textContent = "Wind: " + forecast.wind + " MPH";
+        var humContainer =
+          forecastContainer.querySelector("ul li:nth-child(5)");
+        humContainer.textContent = "Humidity: " + forecast.humidity + "%";
+      }
+
+      //reset form
+      formEl.reset();
+
+      var storedForecastData = JSON.parse(
+        localStorage.getItem("forecastData") || "[]"
+      );
+      storedForecastData.push(forecast);
+      localStorage.setItem("forecastData", JSON.stringify(storedForecastData));
     });
+    buttonList.appendChild(createHistory);
   });
 });
